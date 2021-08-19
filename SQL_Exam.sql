934275b66b20ff7ff57af7cc90da4645f5843499
jpmarquina <marquina.jp016@gmail.com>
2021-08-19 15:47:10 +0800

    SQL_Exam.sql
    SQL Exam

---
+++ SQL_Exam.sql
@@ -0,0 +1,48 @@
+-- 1
+
+SELECT
+CONCAT('T',LPAD(REPLACE(`a`.`id`, ',', ''),11,0)) as 'Id',
+`a`.`nickname` as 'Nickname',
+(CASE
+WHEN `a`.`status` = 1 THEN
+'Active'
+WHEN `a`.`status` = 2 THEN
+'Deactivate'
+ELSE
+'Discontinued'
+END) as 'Status',
+TRIM(TRAILING '/' FROM REPLACE(
+GROUP_CONCAT(
+(CASE
+WHEN `b`.`role` = 1 THEN
+'Trainer'
+WHEN `b`.`role` = 2 THEN
+'Assessor'
+ELSE
+'Staff'
+END), '/'),',','')
+) as 'Roles'
+FROM
+`trn_teacher` `a`
+LEFT OUTER JOIN `trn_teacher_role` `b` ON `a`.`id` = `b`.`teacher_id`
+group by `a`.`id`
+order by `b`.`id` asc
+
+-- 2
+
+SELECT 
+REPLACE(`a`.`id`, ',', '') as 'Id', 
+`a`.`nickname` as 'Nickname',
+(SELECT count(`id`) FROM `trn_time_table` where `teacher_id`=`a`.`id` and `status`=1) as 'Open',
+(SELECT count(`id`) FROM `trn_time_table` where `teacher_id`=`a`.`id` and `status`=3) as 'Reserved',
+(SELECT count(`id`) FROM `trn_evaluation` where `teacher_id`=`a`.`id` and `status`=1) as 'Taught',
+(SELECT count(`id`) FROM `trn_evaluation` where `teacher_id`=`a`.`id` and `result`=2) as 'No Show'
+FROM
+`trn_teacher` `a`
+LEFT OUTER JOIN `trn_teacher_role` `b` ON `a`.`id` = `b`.`teacher_id`
+WHERE
+`a`.`status` in (1,2) and
+`b`.`role` in (1,2)
+GROUP BY `a`.`id;
+
+-- 
